a416e8cd50ce41cb2e011fc19019b465b7463014
esw77277 <51586398+esw77277@users.noreply.github.com>
2019-06-10 18:52:21 +0530

    Add files via upload

---
+++ const.dart
@@ -0,0 +1,14 @@
+void main() {
+  
+  
+
+  
+ //const
+     const pi = 3.14;
+     const double b = 2.21;
+  print(b);
+
+
+
+
+}
---
+++ do_while.dart
@@ -0,0 +1,7 @@
+main() {
+ var i = 1;
+  do {
+    print(i);
+    i++;
+  } while (i <= 10);
+}

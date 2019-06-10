f999aeba4cbc35423ac7f39b1bd4d9f805d89135
esw77277 <51586398+esw77277@users.noreply.github.com>
2019-06-10 16:56:29 +0530

    Create 2_built_in_datatypes.dart

---
+++ 2_built_in_datatypes.dart
@@ -0,0 +1,43 @@
+
+
+void main() {
+
+    // Numbers: int
+  
+    int score = 23;
+  	print(score);
+  
+  // End Number : int
+
+    // Numbers: double
+    double percentage = 93.4 ;
+    var percent = 82.533;
+    double exponents = percentage * percent;
+   
+    print(exponents);
+  // End Numbers: double
+
+    // Strings
+    String name = "Henry";
+    var company = "Google";
+  	print(name);
+  	print(company);
+  // End Strings
+
+    // Boolean
+   bool test; 
+   var istest;
+    
+   test = 12 > 5; 
+   istest=5>12;
+  
+   print(istest);
+   print(test); 
+  
+  //End Boolean
+
+  
+   
+
+ 
+}

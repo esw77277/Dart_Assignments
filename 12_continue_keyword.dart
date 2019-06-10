36484ba66d5255215631b2583bbfb12f554938ff
esw77277 <51586398+esw77277@users.noreply.github.com>
2019-06-10 18:46:39 +0530

    Create 12_continue_keyword.dart

---
+++ 12_continue_keyword.dart
@@ -0,0 +1,10 @@
+void main(){
+   //for loop  
+    for(int i=1;i<=10;i++){  
+        if(i==5){  
+            //using continue statement  
+            continue;//it will skip the rest statement  
+        }  
+       print(i);  
+    }  
+}

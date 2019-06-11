5cb0c600dd2fe97b14013ad05405f4d4fd2ea372
suresh babu <45354387+sureshbabu-snpl@users.noreply.github.com>
2019-06-11 18:00:32 +0530

    Update 15_Basic_List_update.dart

--- 15_Basic_List_update.dart
+++ 15_Basic_List_update.dart
@@ -1,10 +1,10 @@
 void main() {
    List list_name = new List(); // object creation for list
-   list_name.addAll([ramesh,harish,]);// add items to list
+   list_name.addAll(['ramesh','harish']);// add items to list
    
    print('before ${list_name}'); // print the list items
    
-   l.replaceRange(0,1,[suresh,babu]); // replace range of the items in index
+   list_name.replaceRange(0,1,['suresh','babu']); // replace range of the items in index
    
    print('after replace  is ${list_name}'); // print the repalced index items
 }

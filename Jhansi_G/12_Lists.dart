10821cf3b708e9aee35894dbb87cb311a56b394d
Jhansi <jhansigundapaneni121@gmail.com>
2019-06-11 18:42:59 +0530

    Dart

--- Jhansi_G/12_Lists.dart
+++ Jhansi_G/12_Lists.dart
@@ -5,9 +5,19 @@ var list=new List(3);
 list[0]="Dart";
 list[1]="I am not going to leave";
 list[2]="you";
-print(list);
+for (int i=0;i<=list.length;i++)
+{
+if(i==1)
+{
+print("Element at particular index: "+list[i]);
+break;
+}
+
+
+}
 print("  List of Properties");
 print("First Element of list is :"+list.first);
+  print("Last Element of list is :"+list.last);
 print("******************************");
 print("Growable list");
 var Glist= List();
@@ -15,6 +25,9 @@ Glist.add("Dart");
 Glist.add("I am comimg");
 Glist.add("Plz be safe");
 print(Glist);
+  
 print(Glist.isEmpty);
 print(Glist.last);
+  Glist.removeAt(1);
+  print(Glist);
 }
---
+++ Jhansi_G/13_String_Interpolation.dart
@@ -0,0 +1,9 @@
+main()
+{
+  int l=2,b=4;
+f2(l,b);
+}
+ f2(int length,int breadth)
+{
+  print("Area of rectangle is ${length*breadth}");
+}

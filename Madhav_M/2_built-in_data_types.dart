9f35aad1215caba17747c90bf738c6842a2b57b8
Madhava <madhavarao.m@supremesoft.net>
2019-06-11 14:56:22 +0530

    'Madhav_M'

---
+++ Madhav_M/2_built-in_data_types.dart
@@ -0,0 +1,28 @@
+
+
+void main(List<String> arguments) {
+
+    // Numbers: int
+    int score = 23;
+    var count = 23;     // It is inferred as integer automatically by compiler
+    int hexValue = 0xEADEBAEE;
+
+    // Numbers: double
+    double percentage = 93.4;
+    var percent = 82.533;
+    double exponents = 1.444;
+
+    // Strings
+    String name = "Henry";
+    var company = "Google";
+
+    // Boolean
+    bool isValid = true;
+    var isAlive = false;
+
+    print(isValid);
+    print(exponents);
+
+    // NOTE: All data types in Dart are Objects.
+    // Therefore, their initial value is by default 'null'
+}

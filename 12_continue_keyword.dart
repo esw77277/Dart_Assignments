cd87d154c19611aaba341bb972e9de07f513f41a
esw77277 <51586398+esw77277@users.noreply.github.com>
2019-06-11 11:26:28 +0530

    Update 12_continue_keyword.dart

--- 12_continue_keyword.dart
+++ 12_continue_keyword.dart
@@ -2,8 +2,8 @@ void main(){
    //for loop  
     for(int i=1;i<=10;i++){  
         if(i==5){  
-            //using continue statement  
-            continue;//it will skip the rest statement  
+            //continue 
+            continue; //skip  statement  
         }  
        print(i);  
     }  

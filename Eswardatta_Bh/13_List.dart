4c32059e79090de8aa5e93c58c9593c06dfbab79
esw77277 <51586398+esw77277@users.noreply.github.com>
2019-06-12 15:01:40 +0530

    Update 13_List.dart

--- Eswardatta_Bh/13_List.dart
+++ Eswardatta_Bh/13_List.dart
@@ -1,10 +1,16 @@
 void main() {
   List<int> fixedLengthList = new List();
-  
   for (int i = 1; i <= 20; i++) {
     fixedLengthList.add(i);
   }
-  
   print(fixedLengthList);
   print("List length :  ${fixedLengthList.length}");
+  print("First value :  ${fixedLengthList.first}");
+  print("last value :  ${fixedLengthList.last}");
+  print("Reverse :  ${fixedLengthList.reversed}");
+  print("Check :  ${fixedLengthList.contains(27)}");
+  print("Check :  ${fixedLengthList.contains(20)}");
+  print("Print removed item :  ${fixedLengthList.removeLast()-1}");
+  print("Remove index 2 :  ${fixedLengthList.removeAt(2)}");
+  print("Reverse :  ${fixedLengthList.sort()}");
 }

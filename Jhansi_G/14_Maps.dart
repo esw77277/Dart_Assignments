4df956959f657069c7978659bf79c9dd5fb60f0f
JhansiGundapaneni <40382735+JhansiGundapaneni@users.noreply.github.com>
2019-06-12 09:46:13 +0530

    Update 14_Maps.dart

--- Jhansi_G/14_Maps.dart
+++ Jhansi_G/14_Maps.dart
@@ -2,15 +2,15 @@ main()
 {
 var details = {'Usrname':'jhansi','Password':'admin1234'}; 
 details['Email']='Jhansi@gmail.com'; //adding new element
-   print(details.keys); // Keys of map
-   print(details.values); //elements of map
-   var data=new Map(); //Map constructor
-   data['mobile']='9999999999';
-   data['address']='Tenali';
-   details.addAll({'mobile':'9999999999','address':'Tenali'}); // adding <key,value> paits to the map
-   print(details);
-   details.remove('mobile'); // removing element from the map
-   print(details);
-   print("Length of map is ${details.length}"); //length of map
+print(details.keys); // Keys of map
+print(details.values); //elements of map
+var data=new Map(); //Map constructor
+data['mobile']='9999999999';
+data['address']='Tenali';
+details.addAll({'mobile':'9999999999','address':'Tenali'}); // adding <key,value> pairs to the map
+print(details);
+details.remove('mobile'); // removing element from the map
+print(details);
+print("Length of map is ${details.length}"); //length of map
    
 }

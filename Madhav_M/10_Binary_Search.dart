208d8da48548150d2921a5449381f045f9fad026
madhava95 <51589246+madhava95@users.noreply.github.com>
2019-06-13 18:40:31 +0530

    Create 10_Binary_Search.dart

---
+++ Madhav_M/10_Binary_Search.dart
@@ -0,0 +1,35 @@
+void main() 
+{
+  List<int> lst = [0,4,8,9,11,13,18,21,28];
+  print(binarySearch(lst,11));
+}
+
+String binarySearch(List<int> list, int x)
+{
+  
+  int min = 0;
+  int max = list.length-1;
+  
+  while(min <= max )
+  {
+    
+    	int mid = ( (min + max)/2 ).floor();
+    
+    	if(x == list[mid])
+    	{
+      		return "Element $x found at index $mid";
+    	}
+    
+    	else if (x < list[mid])
+    	{
+      		max = list[mid] - 1;
+    	} 
+    
+    	else 
+    	{
+      		min = list[mid] + 1;
+    	}
+  }
+  
+  return  "Element $x not found";
+}

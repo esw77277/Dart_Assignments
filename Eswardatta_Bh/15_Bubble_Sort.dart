ad73d568da60061c217afd857092d8f73e8db3d1
esw77277 <51586398+esw77277@users.noreply.github.com>
2019-06-13 18:03:02 +0530

    Update 15_Bubble_Sort.dart

--- Eswardatta_Bh/15_Bubble_Sort.dart
+++ Eswardatta_Bh/15_Bubble_Sort.dart
@@ -3,6 +3,14 @@ void main()
     List<int> fixedLengthList = [10, 30, 5, 67, 2, 20, 92,8];
     print(Bubble(fixedLengthList));
     print((Bubble(fixedLengthList)).reversed);
+    int n = Bubble(fixedLengthList).length;
+    int element = 0;
+    int result = binary(Bubble(fixedLengthList), 0, n - 1, element);
+  if(result == -1)
+     print("$element is not present in array");
+     else
+     print("$element prasent in array");
+
 }
 List<int> Bubble(List<int> list)
 {
@@ -17,8 +25,22 @@ List<int> Bubble(List<int> list)
                 list[j + 1] = temp;
             }
         }
-        print("step:$i : $list");
+        
     }
   return list;
+}
+
+
+int binary(List , int l, int r, int x)
+{
+   if (r >= l) {
+       int mid = l + (r - l) ~/ 2;
+       if (List[mid] == x)
+           return mid;
+       if (List[mid] > x)
+           return binary(List, l, mid - 1, x);
+       return binary(List, mid + 1, r, x);
+   }
+   return -1;
 }
 
